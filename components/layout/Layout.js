f231ff8c8b809ae9d472c9c6d5d297eba495c843
Amir Hossin Sharifi Fard <amirawade7@gmail.com>
2025-03-23 19:08:12 +0330

    add swiper and card to main project

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,7 +1,5 @@
 import styles from "./Layout.module.css";
 
-<<<<<<< HEAD
-=======
 import Logo from "/public/logo.png";
 
 // icons header
@@ -15,7 +13,6 @@ import {FaTelegramPlane} from "react-icons/fa";
 import {FaTwitter} from "react-icons/fa";
 import {FaWhatsapp} from "react-icons/fa";
 
->>>>>>> 5cec35f (header and footer are complited)
 import Header from "../module/Header";
 import Footer from "../module/Footer";
 const Layout = ({children}) => {
